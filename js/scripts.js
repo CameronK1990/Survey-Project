a6dccbddf83585b5f4d7e498cf9356447a30838b
CameronK1990 <cameron.kronsbein@gmail.com>
2021-08-13 10:56:18 -0700

    WIP business logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,19 @@ function add (question1,question2,question3,question4,question5) {
   return (question1 + question2 + question3 + question4 + question5)
 }
 
+$(document).ready(function() {
+  $("form#survey").submit(function(event) {
+    event.preventDefault();
+    $("").hide();
+    $("").show();
+  });
+  
+  event.preventDefault();
+  let question1 = parseInt($("input:radio[name = question1]:checked").val());
+  let question2 = parseInt($("input:radio[name = question2]:checked").val());
+  let question3 = parseInt($("input:radio[name = question3]:checked").val());
+  let question4 = parseInt($("input:radio[name = question4]:checked").val());
+  let question5 = parseInt($("input:radio[name = question5]:checked").val());
+
+
+}
