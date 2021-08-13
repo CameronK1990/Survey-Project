043ce2513df7f5a88fcd6568f34b8472a08948ab
CameronK1990 <cameron.kronsbein@gmail.com>
2021-08-13 16:22:28 -0700

    WIP removed bad function for animated effect button

--- js/scripts.js
+++ js/scripts.js
@@ -21,15 +21,5 @@ $(document).ready(function() {
     }
 
     $(".survey").hide();
-
-$( "#clickme" ).click(function() {
-  $( "#backend" ).animate({
-    opacity: 0.25,
-    left: "+=50",
-    height: "toggle"
-  }, 5000, function() {
-    // Animation complete.
-  });
-});
   });
 });
