e37d843075ecc0ac70c4066e2ae1879a2744d405
CameronK1990 <cameron.kronsbein@gmail.com>
2021-08-13 11:41:54 -0700

    WIP CSS and logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,21 @@ function add (question1,question2,question3,question4,question5) {
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
-    $("").hide();
-    $("").show();
+    let question1 = parseInt($("input:radio[name = question1]:checked").val());
+    let question2 = parseInt($("input:radio[name = question2]:checked").val());
+    let question3 = parseInt($("input:radio[name = question3]:checked").val());
+    let question4 = parseInt($("input:radio[name = question4]:checked").val());
+    let question5 = parseInt($("input:radio[name = question5]:checked").val());
+    let answer = add(question1,question2,question3,question4,question5)
+
+    if (answer =< 6) {
+      $("#backend").show();
+    } else if (answer = 7) {
+      $("#fullstack").show();
+    } else (answer > 7) {
+      $("#frontend").show();
+    }
+
+    $(".survey").hide();
   });
-  
-  event.preventDefault();
-  let question1 = parseInt($("input:radio[name = question1]:checked").val());
-  let question2 = parseInt($("input:radio[name = question2]:checked").val());
-  let question3 = parseInt($("input:radio[name = question3]:checked").val());
-  let question4 = parseInt($("input:radio[name = question4]:checked").val());
-  let question5 = parseInt($("input:radio[name = question5]:checked").val());
-
-
-}
+});
